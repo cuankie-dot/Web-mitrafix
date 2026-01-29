db60056ea6fe32bf8706c4361604ef3ae03ec9f4
cuankie-dot <anjar.setiyono@gmail.com>
2026-01-29 13:34:37 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ const Footer: React.FC<{ onShowLeads: () => void }> = ({ onShowLeads }) => {
               {/* Logo Footer dengan penanganan Error yang Robust */}
               {!logoError ? (
                 <img 
-                  src="/images/logo.png?v=2" 
+                  src="https://715udgq2n1apqtj8.public.blob.vercel-storage.com/image/fav_ico/web-app-manifest-512x512.png" 
                   alt="Mitrafix Logo" 
                   className="h-12 w-auto bg-white rounded-lg p-2 object-contain"
                   onError={() => setLogoError(true)}
